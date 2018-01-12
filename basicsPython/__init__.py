9968f255a577e92e0554cf569c74756e7b12d385
宿珑 <13947636233@163.com>
2018-01-12 15:32:36 +0800

    update listBasics

---
+++ basicsPython/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/1/12 下午3:28
+# @Author  : Sulong
+# @File    : __init__.py.py
+# @Software: PyCharm
---
+++ dataStructure/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/1/12 下午3:29
+# @Author  : Sulong
+# @File    : __init__.py.py
+# @Software: PyCharm
